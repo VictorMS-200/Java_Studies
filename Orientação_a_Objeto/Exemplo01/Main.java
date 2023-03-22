324aba42090c7f2a0b5927ff3a7166678e2d51aa
Victor <victorm20005@gmail.com>
2023-03-22 01:20:27 -0300

    Criado o projeto Exemplo01

---
+++ Orientação_a_Objeto/Exemplo01/Main.java
@@ -0,0 +1,12 @@
+// Nesse programa, é utilizado Orientação a Objeto do tipo encapsulação
+
+package Orientação_a_Objeto.Exemplo01;
+
+public class Main {
+    public static void main(String[] args) {
+        User usuario1 = new User();
+
+        usuario1.setName("Victor");
+        System.out.println(usuario1.getName());
+    }
+}
---
+++ Orientação_a_Objeto/Exemplo01/User.java
@@ -0,0 +1,13 @@
+package Orientação_a_Objeto.Exemplo01;
+
+public class User {
+    private String name;
+
+    public void setName(String Nome_do_Usuario) {
+        this.name = Nome_do_Usuario;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
