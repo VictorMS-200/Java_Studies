40ab68e92b1adbd5b7ec556b090f1b3124377e63
Victor <victorm20005@gmail.com>
2023-03-22 10:42:36 -0300

    Codido candidato adicionado

--- Orientação_a_Objeto/Exemplo02/Candidato.java
+++ Orientação_a_Objeto/Exemplo02/Candidato.java
@@ -1,5 +1,18 @@
 package Orientação_a_Objeto.Exemplo02;
 
 public class Candidato {
-    
+    private String[] candidato;
+
+    public void setCandidato(String[] InformaçõesCandidato) {
+        this.candidato = InformaçõesCandidato;
+    }
+    public void getCandidato() {
+        for (int i = 0; i < candidato.length; i++) {
+            if (i % 2 == 0) {
+                System.out.print(candidato[i] + ": ");
+            } else {
+                System.out.println(candidato[i]);
+            }
+        }
+    }
 }
